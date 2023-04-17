f98783c434ca22a33717854ef73dd74c628e5cc7
AntonD3 <antonv.dyadyuk@gmail.com>
2023-04-18 00:01:16 +0100

    Formatting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import 'hardhat-typechain'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
 import '@nomiclabs/hardhat-etherscan'
-import "@matterlabs/hardhat-zksync-solc"
+import '@matterlabs/hardhat-zksync-solc'
 
 export default {
   networks: {
