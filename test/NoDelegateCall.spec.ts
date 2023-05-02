76b0e0817fd77a854ad29a6b9e115b5bf809bc8d
vladbochok <vladbochok1@gmail.com>
2023-05-02 07:19:48 +0200

    Add NoDelegateCall test

--- test/NoDelegateCall.spec.ts
+++ test/NoDelegateCall.spec.ts
@@ -1,54 +1,52 @@
-// import { ethers, waffle } from 'hardhat'
-// import { NoDelegateCallTest } from '../typechain/NoDelegateCallTest'
-// import { expect } from './shared/expect'
-// import snapshotGasCost from './shared/snapshotGasCost'
+import { NoDelegateCallTest } from '../typechain/NoDelegateCallTest'
+import { deployContract, getWallets, loadArtifact } from './shared/zkSyncUtils'
+import { expect } from './shared/expect'
+import snapshotGasCost from './shared/snapshotGasCost'
+import { Wallet } from 'zksync-web3'
+import { Contract } from 'ethers'
+import { ZkSyncArtifact } from '@matterlabs/hardhat-zksync-deploy/dist/types'
 
-// describe('NoDelegateCall', () => {
-//   const [wallet, other] = waffle.provider.getWallets()
+describe('NoDelegateCall', () => {
+    let wallet: Wallet;
 
-//   let loadFixture: ReturnType<typeof waffle.createFixtureLoader>
-//   before('create fixture loader', async () => {
-//     loadFixture = waffle.createFixtureLoader([wallet, other])
-//   })
+    before('create fixture loader', async () => {
+        ;[wallet] = getWallets()
+      })
 
-//   const noDelegateCallFixture = async () => {
-//     await deployContract('LiquidityMathTest') as LiquidityMathTest
-//     const noDelegateCallTestFactory = await ethers.getContractFactory('NoDelegateCallTest')
-    
-//     const noDelegateCallTest = (await noDelegateCallTestFactory.deploy()) as NoDelegateCallTest
-//     const minimalProxyFactory = new ethers.ContractFactory(
-//       noDelegateCallTestFactory.interface,
-//       `3d602d80600a3d3981f3363d3d373d3d3d363d73${noDelegateCallTest.address.slice(2)}5af43d82803e903d91602b57fd5bf3`,
-//       wallet
-//     )
-//     const proxy = (await minimalProxyFactory.deploy()) as NoDelegateCallTest
-//     return { noDelegateCallTest, proxy }
-//   }
+  const noDelegateCallFixture = async () => {    
+    const noDelegateCallTestContract = (await deployContract('NoDelegateCallTest'))
+    const noDelegateCallTestArtifact = await loadArtifact('NoDelegateCallTest')
+    const noDelegateCallTest = new Contract(noDelegateCallTestContract.address, noDelegateCallTestArtifact.abi, wallet)
 
-//   let base: NoDelegateCallTest
-//   let proxy: NoDelegateCallTest
+    const proxyTestContract = (await deployContract('ProxyTest', [noDelegateCallTest.address]))
+    const proxy = new Contract(proxyTestContract.address, noDelegateCallTestArtifact.abi, wallet)
+    return { noDelegateCallTest, proxy }
+  }
 
-//   beforeEach('deploy test contracts', async () => {
-//     ;({ noDelegateCallTest: base, proxy } = await loadFixture(noDelegateCallFixture))
-//   })
+  let base: NoDelegateCallTest
+  let proxy: NoDelegateCallTest
 
-//   it('runtime overhead', async () => {
-//     await snapshotGasCost(
-//       (await base.getGasCostOfCannotBeDelegateCalled()).sub(await base.getGasCostOfCanBeDelegateCalled())
-//     )
-//   })
+  beforeEach('deploy test contracts', async () => {
+    ;({ noDelegateCallTest: base, proxy } = await noDelegateCallFixture())
+  })
 
-//   it('proxy can call the method without the modifier', async () => {
-//     await proxy.canBeDelegateCalled()
-//   })
-//   it('proxy cannot call the method with the modifier', async () => {
-//     await expect(proxy.cannotBeDelegateCalled()).to.be.reverted
-//   })
+  it('runtime overhead', async () => {
+    await snapshotGasCost(
+      (await base.getGasCostOfCannotBeDelegateCalled()).sub(await base.getGasCostOfCanBeDelegateCalled())
+    )
+  })
 
-//   it('can call the method that calls into a private method with the modifier', async () => {
-//     await base.callsIntoNoDelegateCallFunction()
-//   })
-//   it('proxy cannot call the method that calls a private method with the modifier', async () => {
-//     await expect(proxy.callsIntoNoDelegateCallFunction()).to.be.reverted
-//   })
-// })
+  it('proxy can call the method without the modifier', async () => {
+    await proxy.canBeDelegateCalled()
+  })
+  it('proxy cannot call the method with the modifier', async () => {
+    await expect(proxy.cannotBeDelegateCalled()).to.be.reverted
+  })
+
+  it('can call the method that calls into a private method with the modifier', async () => {
+    await base.callsIntoNoDelegateCallFunction()
+  })
+  it('proxy cannot call the method that calls a private method with the modifier', async () => {
+    await expect(proxy.callsIntoNoDelegateCallFunction()).to.be.reverted
+  })
+})
